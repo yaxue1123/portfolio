22b77c6633fdb3c9c4baef54d0ab713876d59e1c
yaxueguo <yaxue@live.unc.edu>
2019-01-14 23:55:22 -0500

    edit content

--- src/Js/all.js
+++ src/Js/all.js
@@ -35,18 +35,18 @@ function showSlides(n) {
 }
 
 // ----------------- show and hide --------------------
-document.querySelectorAll(".more-less").forEach(function(el){
-    el.addEventListener("click",function(e){
-        var el = e.currentTarget.nextElementSibling;
-        if (e.currentTarget.textContent == "See more »") {
-            el.style.display = 'block';
-            e.currentTarget.textContent = "See less »";
-        } else {
-            el.style.display = 'none';
-            e.currentTarget.textContent = "See more »";
-        }
-    });
-});
+// document.querySelectorAll(".more-less").forEach(function(el){
+//     el.addEventListener("click",function(e){
+//         var el = e.currentTarget.nextElementSibling;
+//         if (e.currentTarget.textContent == "See more »") {
+//             el.style.display = 'block';
+//             e.currentTarget.textContent = "See less »";
+//         } else {
+//             el.style.display = 'none';
+//             e.currentTarget.textContent = "See more »";
+//         }
+//     });
+// });
 
 // ----------------- drop-down menu --------------------
 document.querySelectorAll(".dropbtn").forEach(function(el){
