7fdfd6ba4b9dd0c114c3fb6e0855c0d4318e4ad9
yaxueguo <yaxue@live.unc.edu>
2019-01-16 01:50:59 -0500

    combined js using jquery

--- src/Js/all.js
+++ src/Js/all.js
@@ -1,66 +1,131 @@
-// ----------------- slide show --------------------
-var slideIndex = 1;
-showSlides(slideIndex);
+// when the DOM is ready, execute the JQuery code
+$(document).ready(function () {
+    // ----------------- slide show --------------------
+    $(".prev").click(function (e) {
+        plusSlides(-1);
+    })
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    $(".next").click(function (e) {
+        plusSlides(1);
+    })
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    $(".dot").click(function (e) {
+        let id = $(this).attr("id").substr(4);
+        currentSlide(id);
+    })
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-  try {  
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].className += " active";
-  }
-  catch(err) {
-    console.log("Not homepage.");
-  }
-}
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-// ----------------- show and hide --------------------
-// document.querySelectorAll(".more-less").forEach(function(el){
-//     el.addEventListener("click",function(e){
-//         var el = e.currentTarget.nextElementSibling;
-//         if (e.currentTarget.textContent == "See more »") {
-//             el.style.display = 'block';
-//             e.currentTarget.textContent = "See less »";
-//         } else {
-//             el.style.display = 'none';
-//             e.currentTarget.textContent = "See more »";
-//         }
-//     });
-// });
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
 
-// ----------------- drop-down menu --------------------
-document.querySelectorAll(".dropbtn").forEach(function(el){
-    el.addEventListener("click",function(e){
-        var el = e.currentTarget.nextElementSibling;
-
-        if (el.classList.contains("dropdown-content-show")){
-            el.classList.remove("dropdown-content-show");
-            el.classList.add("dropdown-content");
-        } else{
-            el.classList.remove("dropdown-content");
-            el.classList.add("dropdown-content-show");
+    function showSlides(n) {
+        let i;
+        let slides = $(".slides");
+        let dots = $(".dot");
+        if (n > slides.length) {
+            slideIndex = 1;
+        } else if (n < 1) {
+            slideIndex = slides.length;
         }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        try {
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+        catch (err) {
+        }
+    }
+
+    // ------------- education, internship and leadership ---------
+    // control hide and show effect.
+    $(".more-less").click(function (e) {
+        let id = "detail-" + $(this).attr("id");
+        // if going to show, hide all other showing items first.
+        if ($("#" + id).css("display") == "none") {
+            $(".hide").css("display", "none");
+        }
+        $("#" + id).toggle();
+    })
+
+    // -------------------------- project----------------------------
+    // --------- show Back to Top Button ----------
+    let scrollTrigger = 100, // px
+        showBackToTop = function () {
+            let scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+
+    $(window).on('scroll', function () {
+        showBackToTop();
     });
-});
+
+    // ---------- scroll to position -------
+    // Scroll whole page to top when clicking the button.
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+
+    // scroll to top of user's selection of a project.
+    // when user selects a category.
+    $('.project-sidebar h2').on('click', function (e) {
+        // Create the id of element to be scrolled.
+        let id = $(this).attr("id") + '-scroll';
+        let top = $('#' + id).offset().top;
+        $('html,body').animate({
+            scrollTop: top - 100
+        }, 400);
+    });
+    // when user selects a specific project.
+    $('.project-sidebar p').on('click', function (e) {
+        // Decide which row, 3 items per row.
+        let row = parseInt(($(this).attr("id").split('-')[1] - 1) / 3) + 1;
+        // Create the id of element to be scrolled.
+        let id = $(this).attr("id").substr(0, 2) + '-scroll-' + row;
+        let top = $('#' + id).offset().top;
+        $('html,body').animate({
+            scrollTop: top - 100
+        }, 400);
+    });
+
+    // ----------------- mobile view --------------------------------
+    // ----------------- drop-down menu -----------------------------
+    $(".dropbtn").on('click', function (e) {
+        let el = $(this).next();
+        if (el.hasClass("dropdown-content-show")) {
+            el.removeClass("dropdown-content-show");
+            el.addClass("dropdown-content");
+        } else {
+            el.removeClass("dropdown-content");
+            el.addClass("dropdown-content-show");
+        }
+    })
+})
+
+
+
+
+
 
 
